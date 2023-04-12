35a89596a8789ac347c38dacb397462b06d34fe0
Ethan Gray <74423016+eef-g@users.noreply.github.com>
2023-04-12 14:35:30 -0700

    Button Scaling & Rework
    -== Brian did this commit, just on my laptop ==-
    * Nodes now are able to scale with the resolution & UI elements are anchored to different areas
    
    * Nodes are now stored with a Grid Layout Group
    
    -- Future Plan:
    * Drag Handler will be with the manager instead
    * When dragging, disable the current node in the grid layout and spawn an identical node that is able to be dragged. Whenever the fake node is over the grid layout, spawn an empty gameobject where it is to mimic it being moved into that slot. When let go, move the original gameobject to where the empty one is and delete the empty object as well as the temp clone you made
    
    Co-Authored-By: Min Jun Kim <BrianMJK@Gmail.com>

--- Assets/NodeManager.cs
+++ Assets/NodeManager.cs
@@ -5,44 +5,40 @@ using UnityEngine.UI;
 
 public class NodeManager : MonoBehaviour
 {
-    Button node_button, compile_button;
-    GameObject node_container;
 
-    [SerializeField]
-    GameObject node_prefab;    
+    [Header("Properties")]
+    [SerializeField] Button nodeButton; 
+    [SerializeField] Button compileButton;
+    [SerializeField] Transform nodeContainer;
+
+    [SerializeField] GameObject nodePrefab;    
     // Start is called before the first frame update
     void Start()
-    {
-        foreach(Transform child in transform)
-        {
-            if(child.gameObject.name == "NodeButton")
-            {
-                node_button = child.GetComponent<Button>();
-                node_button.onClick.AddListener(delegate { CreateNode(); });
-            }
-            if(child.gameObject.name == "CompileButton")
-            {
-                compile_button = child.GetComponent<Button>();
-                compile_button.onClick.AddListener(delegate { Compile(); });
-            }
-            if(child.gameObject.name == "Nodes")
-            {
-                node_container = child.gameObject;
-            }
-        }
+    {  
+            nodeButton.onClick.AddListener(CreateNode);  
+            compileButton.onClick.AddListener(Compile);  
+
+            print(transform.Find("BG"));
     }
 
     public void CreateNode()
     {
-        GameObject node = Instantiate(node_prefab);
-        node.transform.SetParent(node_container.transform);
-        node.transform.localScale = new Vector3(1.2917f, 1.2917f, 1.2917f);
-        node.transform.GetComponent<RectTransform>().position = new Vector3(-4.40f, 15, 0);
+        Vector2 position = Vector2.zero;
+        Transform lastNode;
+        if(nodeContainer.childCount != 0)
+        {
+            lastNode = nodeContainer.GetChild(nodeContainer.childCount - 1); 
+            position = lastNode.position;
+        }        
+
+        
+
+        GameObject node = Instantiate(nodePrefab, nodeContainer);   
     }
 
     public void Compile()
     {
-        foreach(Transform child in node_container.transform)
+        foreach(Transform child in nodeContainer.transform)
         {
             Debug.Log(child.gameObject.name);
         }
